55d90f37dea8abf7cf80e0b2f91f91b746c5c7d4
KamiyamaShun <76637787+KamiyamaShun@users.noreply.github.com>
2021-02-10 16:38:54 +0900

    set webview function

--- webView/ViewController.swift
+++ webView/ViewController.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,WKNavigationDelegate {
 
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+    
+    @IBOutlet weak var toolbar: UIToolbar!
+    
+    var webView = WKWebView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        indicator.isHidden = true
+        
+        webView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height - toolbar.frame.size.height)
+        
+        view.addSubview(webView)
+        
+        webView.navigationDelegate = self
+        
+        let url = URL(string: "https://www.youtube.com/")
+        let request = URLRequest(url: url!)
+        webView.load(request)
+        
+        indicator.layer.zPosition = 2
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        indicator.isHidden = true
+        indicator.stopAnimating()
+    }
+    
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        indicator.isHidden = false
+        indicator.startAnimating()
     }
 
-
+    @IBAction func back(_ sender: Any) {
+        webView.goBack()
+    }
+    
+    @IBAction func go(_ sender: Any) {
+        webView.goForward()
+    }
+    
 }
 
